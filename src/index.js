4295e9a376cf28169eff0878e864931404ab3bc3
Christopher Gallego <ch.gallego06@gmail.com>
2024-06-19 22:47:55 -0500

    Updated

--- src/index.js
+++ src/index.js
@@ -9,26 +9,26 @@ app.use(bodyParser.json());
 
 // Define a list of allowed origins
 const allowedOrigins = [
-  'https://www.acososexualyopal.com', // Typically you would not specify the port if serving over https
-  'http://18.191.56.149:3000',         // Replace with your actual server IP and port
-  'http://172.31.45.244:3000',         // Replace with your actual server IP and port
-  'http://localhost:3000'              // Localhost for development
+  'https://www.acososexualyopal.com',
+  'http://18.191.56.149:3000',
+  'http://172.31.45.244:3000',
+  'http://localhost:3000'
 ];
 
 const corsOptions = {
   origin: (origin, callback) => {
     if (allowedOrigins.includes(origin) || !origin) {
-      // Allow requests with no origin (like curl requests or same-origin)
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
   },
-  methods: 'GET,POST,OPTIONS',
-  allowedHeaders: 'Content-Type,Authorization',
+  methods: ['GET', 'POST', 'OPTIONS'], // Explicitly specify methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Explicitly specify allowed headers
   credentials: true
 };
 
+// Enable CORS with preflight requests handling
 app.use(cors(corsOptions));
 
 // Initialize SQLite database
@@ -49,7 +49,7 @@ const db = new sqlite3.Database('./geoacoso.sqlite', (err) => {
 // Handle preflight requests
 app.options('*', cors(corsOptions));
 
-// Get all coordinates
+// Define routes
 app.get('/coordinates', (req, res) => {
   db.all('SELECT * FROM coordinates', [], (err, rows) => {
     if (err) {
@@ -60,7 +60,6 @@ app.get('/coordinates', (req, res) => {
   });
 });
 
-// Add a new coordinate
 app.post('/coordinates', (req, res) => {
   console.log(`New report captured at ${new Date()}`);
   const { latitude, longitude } = req.body;
@@ -81,6 +80,7 @@ app.post('/coordinates', (req, res) => {
   );
 });
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
